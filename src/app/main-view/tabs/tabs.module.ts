d0b902dd366903d902446ab0c53c56b5364c70e6
Guyutongxue <guyutongxue@163.com>
2021-01-09 21:53:33 +0800

    feat(tabs): add tabs

--- src/app/main-view/tabs/tabs.module.ts
+++ src/app/main-view/tabs/tabs.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from '../../app-routing.module'
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { PlusOutline, CloseOutline } from '@ant-design/icons-angular/icons';
+
 import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
 import { TabsComponent } from './tabs/tabs.component'
 import { EditorComponent } from './editor/editor.component';
@@ -14,6 +17,7 @@ import { EditorComponent } from './editor/editor.component';
     AppRoutingModule,
     FormsModule,
     NzTabsModule,
+    NzIconModule.forChild([PlusOutline, CloseOutline]),
     MonacoEditorModule,
   ],
   exports: [TabsComponent]
--- src/app/main-view/tabs/tabs/tabs.component.ts
+++ src/app/main-view/tabs/tabs/tabs.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.scss']
 })
 export class TabsComponent implements OnInit {
+  selectedIndex: number = 0;
+  tabs = [1, 2, 3];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  newTab() { }
+  closeTab( e: { index: number }) { }
 }
