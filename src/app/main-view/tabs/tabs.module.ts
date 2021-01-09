43f6420c5d554c4bc78aed7911812737caf8a317
Guyutongxue <guyutongxue@163.com>
2021-01-09 21:24:15 +0800

    structure(main-view): introduce wrapper

--- src/app/main-view/tabs/tabs.module.ts
+++ src/app/main-view/tabs/tabs.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../../app-routing.module'
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
+import { TabsComponent } from './tabs/tabs.component'
 import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
-  declarations: [EditorComponent],
+  declarations: [EditorComponent, TabsComponent],
   imports: [
     CommonModule,
+    AppRoutingModule,
     FormsModule,
     NzTabsModule,
     MonacoEditorModule,
-  ]
+  ],
+  exports: [TabsComponent]
 })
 export class TabsModule { }
