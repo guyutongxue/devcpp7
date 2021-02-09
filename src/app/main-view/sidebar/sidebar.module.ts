4a5c818a4a0f410f483a4e54a0850b3ddbb29813
Guyutongxue <guyutongxue@163.com>
2021-02-09 14:25:58 +0800

    feat(watch): basic local var

--- src/app/main-view/sidebar/sidebar.module.ts
+++ src/app/main-view/sidebar/sidebar.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NzTreeViewModule } from 'ng-zorro-antd/tree-view';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
 import { OutlineComponent } from './outline/outline.component';
+import { WatchComponent } from './watch/watch.component';
 
 @NgModule({
-  declarations: [ OutlineComponent ],
+  declarations: [ OutlineComponent, WatchComponent ],
   imports: [
     CommonModule,
     NzTreeViewModule,
+    NzListModule,
+    NzTagModule,
     NzIconModule
   ],
-  exports: [ OutlineComponent ]
+  exports: [ OutlineComponent, WatchComponent ]
 })
 export class SidebarModule { }
--- src/app/main-view/sidebar/watch/watch.component.ts
+++ src/app/main-view/sidebar/watch/watch.component.ts
@@ -1,7 +1,128 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { CollectionViewer, DataSource, SelectionChange } from '@angular/cdk/collections';
+import { FlatTreeControl, TreeControl } from '@angular/cdk/tree';
+import { BehaviorSubject, merge, Observable, of } from 'rxjs';
+import { delay, map, tap } from 'rxjs/operators';
 import { GdbArray } from 'tsgdbmi';
 import { DebugService } from '../../../services/debug.service';
+
+interface FlatNode {
+  expandable: boolean;
+  id: number;
+  label: string;
+  level: number;
+  loading?: boolean;
+}
+
+const TREE_DATA: FlatNode[] = [
+  {
+    id: 0,
+    label: 'Expand to load',
+    level: 0,
+    expandable: true
+  },
+  {
+    id: 1,
+    label: 'Expand to load',
+    level: 0,
+    expandable: true
+  }
+];
+
+function getChildren(node: FlatNode): Observable<FlatNode[]> {
+  return of([
+    {
+      id: Date.now(),
+      label: `Child Node (level-${node.level + 1})`,
+      level: node.level + 1,
+      expandable: true
+    },
+    {
+      id: Date.now(),
+      label: `Child Node (level-${node.level + 1})`,
+      level: node.level + 1,
+      expandable: true
+    },
+    {
+      id: Date.now(),
+      label: `Leaf Node (level-${node.level + 1})`,
+      level: node.level + 1,
+      expandable: false
+    }
+  ]).pipe(delay(500));
+}
+
+
+class DynamicDatasource implements DataSource<FlatNode> {
+  private flattenedData: BehaviorSubject<FlatNode[]>;
+  private childrenLoadedSet = new Set<FlatNode>();
+
+  constructor(private treeControl: TreeControl<FlatNode>, initData: FlatNode[]) {
+    this.flattenedData = new BehaviorSubject<FlatNode[]>(initData);
+    treeControl.dataNodes = initData;
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<FlatNode[]> {
+    const changes: Observable<any>[] = [
+      collectionViewer.viewChange,
+      this.treeControl.expansionModel.changed.pipe(tap(change => this.handleExpansionChange(change))),
+      this.flattenedData
+    ];
+    return merge(changes).pipe(
+      map(() => {
+        return this.expandFlattenedNodes(this.flattenedData.getValue());
+      })
+    );
+  }
+
+  expandFlattenedNodes(nodes: FlatNode[]): FlatNode[] {
+    const treeControl = this.treeControl;
+    const results: FlatNode[] = [];
+    const currentExpand: boolean[] = [];
+    currentExpand[0] = true;
+
+    nodes.forEach(node => {
+      let expand = true;
+      for (let i = 0; i <= treeControl.getLevel(node); i++) {
+        expand = expand && currentExpand[i];
+      }
+      if (expand) {
+        results.push(node);
+      }
+      if (treeControl.isExpandable(node)) {
+        currentExpand[treeControl.getLevel(node) + 1] = treeControl.isExpanded(node);
+      }
+    });
+    return results;
+  }
+
+  handleExpansionChange(change: SelectionChange<FlatNode>): void {
+    if (change.added) {
+      change.added.forEach(node => this.loadChildren(node));
+    }
+  }
+
+  loadChildren(node: FlatNode): void {
+    if (this.childrenLoadedSet.has(node)) {
+      return;
+    }
+    node.loading = true;
+    getChildren(node).subscribe(children => {
+      node.loading = false;
+      const flattenedData = this.flattenedData.getValue();
+      const index = flattenedData.indexOf(node);
+      if (index !== -1) {
+        flattenedData.splice(index + 1, 0, ...children);
+        this.childrenLoadedSet.add(node);
+      }
+      this.flattenedData.next(flattenedData);
+    });
+  }
+
+  disconnect(): void {
+    this.flattenedData.complete();
+  }
+}
 
 @Component({
   selector: 'app-watch',
@@ -11,12 +132,19 @@ import { DebugService } from '../../../services/debug.service';
 export class WatchComponent implements OnInit {
 
   localVariables$: Observable<GdbArray>;
+  isDebugging$: Observable<boolean>;
 
-  constructor(private debugService: DebugService) { 
-  }
+  constructor(private debugService: DebugService) { }
 
   ngOnInit(): void {
     this.localVariables$ = this.debugService.localVariables$;
+    this.isDebugging$ = this.debugService.isDebugging;
   }
+  treeControl = new FlatTreeControl<FlatNode>(
+    node => node.level,
+    node => node.expandable
+  );
+  dataSource = new DynamicDatasource(this.treeControl, TREE_DATA);
 
+  hasChild = (_: number, node: FlatNode) => node.expandable;
 }
